68a8c57da5c1ebe4246acf6dc02d0d6c94a2f3ed
Andr3xss <andresdavid2017ad@gmail.com>
2024-02-21 11:55:27 -0500

    Avance

--- backend/conexion.php
+++ backend/conexion.php
@@ -1,11 +1,11 @@
 <?php
-    $con=mysqli_connect("localhost","root","","quizz");
+    $conexion =mysqli_connect("localhost","root","","quizz","3306");
 
     if(mysqli_connect_errno()){
         echo "Error al conectar a la base de datos".mysqli_connect_error();
      }
      else{
-        echo"Conexion Exitosa";
+        echo"Conexion Exitosa"; 
      }
 
      mysqli_query($con,"SET NAMES 'utf8'");
